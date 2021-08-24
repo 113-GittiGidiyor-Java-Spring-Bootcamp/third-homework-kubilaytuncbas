857659788bc2663effe21aa6f168cc1db6392012
Kubilay Tunçbaş <tuncbaskubilay@gmail.com>
2021-08-24 13:27:57 +0300

    ...

---
+++ patikaodevv3/src/main/java/com/example/patikaodevv3/Patikaodevv3Application.java
@@ -0,0 +1,12 @@
+package com.example.patikaodevv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class Patikaodevv3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Patikaodevv3Application.class, args);
+    }
+
+}
